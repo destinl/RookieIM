f0cd6a89b112fb7c481e41072f9088e52ec774a4
destinal <l17305042002@163.com>
2024-06-17 18:37:18 +0800

    忽略配置文件

---
+++ rookie-im-server/src/main/java/com/rookie/im/Application.java
@@ -0,0 +1,17 @@
+package com.rookie.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @Description: TODO
+ * @Author: ls
+ * @Date: 2024/6/1718:11
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
